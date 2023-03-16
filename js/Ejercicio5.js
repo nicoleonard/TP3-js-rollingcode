2f30910f49f18ad8039c10f735c6d084275e2ba3
nicoleonard <nikolasleonard@gmail.com>
2023-03-16 05:57:20 -0300

    ejercicio resuelto

--- js/Ejercicio5.js
+++ js/Ejercicio5.js
@@ -1,1 +1,15 @@
 /* Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
+
+function mayusminus(cadena){
+    if(cadena.includes(cadena.toLowerCase())){
+        return "la cadena esta formada por minusculas"
+    }
+    if(cadena.includes(cadena.toUpperCase())){
+        return "la cadena esta formada por mayusculas"
+    }
+    return "la cadena esta formada por mayusculas y minusculas";
+}
+
+console.log(mayusminus("TODO MAYUS"));
+console.log(mayusminus("todo minus"));
+console.log(mayusminus("MAYUS minus"));
