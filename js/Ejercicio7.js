b8f2ce0330a5fe8e408a8a1e8780d445f4ce0cf2
nicoleonard <nikolasleonard@gmail.com>
2023-03-16 06:14:32 -0300

    ejercicio resuelto

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,1 +1,12 @@
 /* Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.*/
+
+function tablaMultiplicar(entero){
+    let html = `<table class="table"><thead><tr><th class="col p-1">Numero</th><th class="col p-1">Multiplo</th><th class="col p-1">Resultado</th></thead><tbody>`;
+    for(i=1;i<=10;i++){
+        html = html.concat(`<tr><td>${entero}</td><td>${i}</td><td>${entero*i}</td></tr>`)
+    }
+    html = html.concat(`</tbody></table>`);
+    return html;
+}
+
+document.write(tablaMultiplicar(parseInt(prompt("ingrese un numero entero para hacer su tabla de multiplicar"))));
