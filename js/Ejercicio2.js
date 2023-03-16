a1b7069e903879f496478ba3e820f06462689f7f
nicoleonard <nikolasleonard@gmail.com>
2023-03-16 04:08:38 -0300

    ejercicio resuelto

--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -6,3 +6,25 @@ Añade en última posición la ciudad de París.
 Escribe por pantalla el elemento que ocupa la segunda posición.
 Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
  */
+
+let solicitud = "";
+let arreglo = [];
+
+do{
+    solicitud = prompt("Ingrese el nombre de una ciudad para almacenar en un arreglo");
+    arreglo.push(solicitud);
+
+}while(confirm("Desea agregar otro mas?"));
+
+console.log(arreglo.length);
+
+let html = `<section><ul class="list-group list-group-flush">`;
+for(let i=0; i<arreglo.length;i++){
+    if(i==0 || i == 2 || i==arreglo.length-1) html=html.concat("",`<li class="list-group-item">${arreglo[i]}</li>`);
+}
+html=html.concat(`</ul></section>`);
+document.write(html);
+
+arreglo.push("París");
+document.write(`<section> <p class="px-3"> ${arreglo[1]} </p> </section>`);
+arreglo.splice(1,"Barcelona");
