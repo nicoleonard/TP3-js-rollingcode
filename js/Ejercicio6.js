cc2b36d449e418b0355f47d4eea1edad1fdf83ac
nicoleonard <nikolasleonard@gmail.com>
2023-03-16 06:03:10 -0300

    ejercicio resuelto

--- js/Ejercicio6.js
+++ js/Ejercicio6.js
@@ -2,3 +2,12 @@
 
 La fórmula del perímetro  es p = 2*(a +b)
 */
+
+function perimetroCuadrilatero(ladoA,ladoB){
+    return "el perimetro del cuadrilatero es: "+ 2*(ladoA+ladoB);
+}
+
+let A = parseInt(prompt("ingrese un numero entero para un lado del cuadrilatero"));
+let B = parseInt(prompt("ingrese un numero entero para el otro lado del cuadrilatero"));
+
+document.write(perimetroCuadrilatero(A,B));
